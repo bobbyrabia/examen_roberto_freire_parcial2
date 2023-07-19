d8046e3a96b3179f366759bfbdcb63a84bcb4fb8
bobbyrabia <rpfreire1@espe.edu.ec>
2023-07-19 01:08:55 -0500

    initial configuration

---
+++ src/main/java/ec/edu/espe/arquitectura/examen_roberto_freire_parcial_II/ExamenRobertoFreireParcialIiApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen_roberto_freire_parcial_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenRobertoFreireParcialIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenRobertoFreireParcialIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/examen_roberto_freire_parcial_II/ExamenRobertoFreireParcialIiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen_roberto_freire_parcial_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRobertoFreireParcialIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
